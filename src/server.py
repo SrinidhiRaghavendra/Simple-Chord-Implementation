cf33485f4e099ef920bda08d39788ffb5d36849b
Srinidhi Raghavendra <sraghav2@cs.binghamton.edu>
2020-11-02 18:11:25 -0500

    Fixed: findPred and closestPred methods fixed. TODO: Test writeFile and readFile

--- src/server.py
+++ src/server.py
@@ -24,7 +24,7 @@ class ProcessorHandler:
         sha256 = hashlib.sha256()
         sha256.update((self.ipaddr+":"+self.port).encode('utf-8'))
         self.id = sha256.hexdigest()
-        self.node = ttypes.NodeID(self.id, self.ipaddr, self.port)
+        self.node = ttypes.NodeID(self.id, self.ipaddr, int(self.port))
 
     def writeFile(self, rFile):
         rFileMeta = rFile.meta
@@ -48,11 +48,13 @@ class ProcessorHandler:
 
 
     def readFile(self, filename):
-        if(filename not in self.files or fileid <= self.pred or fileid > self.id):
+        if(filename not in self.files):
             raise ttypes.SystemException("Given filename not present in this node")
         sha256 = hashlib.sha256()
         sha256.update(filename.encode('utf-8'))
         fileid = sha256.hexdigest()
+        if(fileid <= self.pred.id or fileid > self.id):
+            raise ttypes.SystemException("Given filename not present in this node")
         readFile = open(filename, "r")
         content = readFile.read()
         readFile.close()
@@ -68,26 +70,34 @@ class ProcessorHandler:
         if(self.fingerTable is None or len(self.fingerTable) == 0):
             raise ttypes.SystemException("No finger table present for this node")
         if(key == self.id):
-            return self
+            return self.node
         pred = self.findPred(key);
-        print(type(pred))
-        #ask pred node for its succesor and return it
-        transport = TSocket.TSocket(pred.ip, int(pred.port))
-        transport = TTransport.TBufferedTransport(transport)
-        protocol = TBinaryProtocol.TBinaryProtocol(transport)
-        client = FileStore.Client(protocol)
-        transport.open()
-        succ = client.getNodeSucc()
-        transport.close()
-        return succ
+        if(pred.id == self.id):
+            return self.getNodeSucc()
+        else:
+            #ask pred node for its succesor and return it
+            transport = TSocket.TSocket(pred.ip, int(pred.port))
+            transport = TTransport.TBufferedTransport(transport)
+            protocol = TBinaryProtocol.TBinaryProtocol(transport)
+            client = FileStore.Client(protocol)
+            transport.open()
+            succ = client.getNodeSucc()
+            transport.close()
+            return succ
 
     def findPred(self, key):
         if(self.fingerTable is None or len(self.fingerTable) == 0):
             raise ttypes.SystemException("No finger table present for this node")
-        if(key <= self.id or key > self.fingerTable[0].id):
+        if(key > self.id and key >= self.fingerTable[0].id and self.fingerTable[0].id < self.id):
+            #This means key is between the largest node and the smallest node but it is larger than the largest node
+            return self.node
+        elif(key < self.id and key <= self.fingerTable[0].id and self.fingerTable[0].id < self.id):
+            #this means key is between the largest node and the smallest node but it is smaller than the smallest node
+            return self.node
+        elif(key <= self.id or key > self.fingerTable[0].id):
             #ask the closest known pred node
             predNode = self.closestPred(key)
-            if(predNode != self):
+            if(predNode.id != self.id):
                 #ask the pred node for the actual pred node
                 transport = TSocket.TSocket(predNode.ip, int(predNode.port))
 
@@ -102,24 +112,32 @@ class ProcessorHandler:
                 transport.close()
                 return pred
             else:
-                return self
+                return self.node
         else:
-            return self
+            return self.node
 
     def closestPred(self, key):
-        if(key < self.id):
+        '''if(key < self.id):
             prev = self.node
             for i in range(0,len(self.fingerTable)):
                 if(self.fingerTable[i].id > key):
                     break
                 else:
                     prev = self.fingerTable[i]
-            return prev
+            return prev'''
+        if(self.id == self.fingerTable[0].id):
+            return self.node
         else:
-            for i in range(len(self.fingerTable)-1,-1,-1):
-                if(self.fingerTable[i].id < key):
-                    return self.fingerTable[i]
-            return self
+            if(key > self.id):
+                prev = self.node
+                for i in range(len(self.fingerTable)):
+                    if(self.fingerTable[i].id >= key):
+                        break
+                    else:
+                        prev = self.fingerTable[i]
+                return prev
+            else:
+                return self.getNodeSucc()
 
     def getNodeSucc(self):
         if(self.fingerTable is None or len(self.fingerTable) == 0):
